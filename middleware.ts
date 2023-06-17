22e885235943194b8db568b51381922096f86ca7
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-18 00:58:04 +0900

    ユーザーのセッションを更新する

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,30 @@
-import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
-import type { NextRequest } from 'next/server'
-import type { Database } from './utils/database.types'
+import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
-// ミドルウェア
+import type { NextRequest } from "next/server";
+import type { Database } from "./utils/database.types";
+
+/*
+|--------------------------------------------------------------------------
+| ユーザーのセッションを更新する
+|--------------------------------------------------------------------------
+*/
 export const middleware = async (req: NextRequest) => {
-  const res = NextResponse.next()
+  const res = NextResponse.next();
 
-  const supabase = createMiddlewareSupabaseClient<Database>({ req, res })
+  const supabase = createMiddlewareSupabaseClient<Database>({ req, res });
 
   const {
     data: { session },
-  } = await supabase.auth.getSession()
+  } = await supabase.auth.getSession();
 
   // 未認証状態で新規投稿画面に遷移した場合は、ログイン画面にリダイレクト
-  if (!session && req.nextUrl.pathname.startsWith('/blog/new')) {
-    const redirectUrl = req.nextUrl.clone()
-    redirectUrl.pathname = '/auth/login'
-    return NextResponse.redirect(redirectUrl)
+  if (!session && req.nextUrl.pathname.startsWith("/blog/new")) {
+    const redirectUrl = req.nextUrl.clone();
+    redirectUrl.pathname = "/auth/login";
+    return NextResponse.redirect(redirectUrl);
   }
 
-  return res
-}
+  return res;
+};
