6b2eea25a0f1d3e0ca9398665eab83b3feed637f
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-18 01:16:38 +0900

    状態管理 Zustandの設定

--- store/index.ts
+++ store/index.ts
@@ -1,22 +1,26 @@
-// Zustand Reactの状態管理ライブラリ
-// https://github.com/pmndrs/zustand
-import { create } from 'zustand'
+/*
+|--------------------------------------------------------------------------
+| Zustand Reactの状態管理ライブラリ
+| https://github.com/pmndrs/zustand
+|--------------------------------------------------------------------------
+*/
+import { create } from "zustand";
 
 type User = {
-  id: string | undefined
-  email: string | undefined
-}
+  id: string | undefined;
+  email: string | undefined;
+};
 
 type State = {
-  user: User
-  setUser: (payload: User) => void
-}
+  user: User;
+  setUser: (payload: User) => void;
+};
 
 const useStore = create<State>((set) => ({
   // 初期値
-  user: { id: '', email: '' },
+  user: { id: "", email: "" },
   // アップデート
   setUser: (payload) => set({ user: payload }),
-}))
+}));
 
-export default useStore
+export default useStore;
