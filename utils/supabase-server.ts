b864a61758e8c15f7b6ab3a0f7337755188654be
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-18 01:16:21 +0900

    supabase server設定

--- utils/supabase-server.ts
+++ utils/supabase-server.ts
@@ -1,10 +1,16 @@
-// Supabase Client
-import { headers, cookies } from 'next/headers'
-import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import type { Database } from '../utils/database.types'
+/*
+|--------------------------------------------------------------------------
+| Supabase Client
+|--------------------------------------------------------------------------
+*/
+import { cookies, headers } from "next/headers";
+
+import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
+
+import type { Database } from "../utils/database.types";
 
 export const createClient = () =>
   createServerComponentSupabaseClient<Database>({
     headers,
     cookies,
-  })
+  });
