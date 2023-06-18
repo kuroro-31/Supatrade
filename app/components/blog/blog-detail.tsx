bfd7523b8f2fb26894febcced39e9babc6766467
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-18 10:56:58 +0900

    編集画面 パス修正

--- app/components/blog/blog-detail.tsx
+++ app/components/blog/blog-detail.tsx
@@ -1,68 +1,69 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react'
-import { format } from 'date-fns'
-import { useRouter } from 'next/navigation'
-import { useSupabase } from '../supabase-provider'
-import { TrashIcon, PencilSquareIcon } from '@heroicons/react/24/outline'
+import { format } from "date-fns";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-import Link from 'next/link'
-import useStore from '../../../store'
-import Image from 'next/image'
-import Loading from '../../loading'
-import BlogComment from './blog-comment'
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
 
-import type { BlogDetailType } from '../../../utils/blog.types'
+import useStore from "../../../store";
+import Loading from "../../loading";
+import { useSupabase } from "../supabase-provider";
+import BlogComment from "./blog-comment";
+
+import type { BlogDetailType } from "../../../utils/blog.types";
 type PageProps = {
-  blog: BlogDetailType
-}
+  blog: BlogDetailType;
+};
 
 // ブログ詳細
 const BlogDetail = ({ blog }: PageProps) => {
-  const { supabase } = useSupabase()
-  const router = useRouter()
-  const { user } = useStore()
-  const [myBlog, setMyBlog] = useState(false)
-  const [loading, setLoading] = useState(false)
-  const [login, setLogin] = useState(false)
+  const { supabase } = useSupabase();
+  const router = useRouter();
+  const { user } = useStore();
+  const [myBlog, setMyBlog] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [login, setLogin] = useState(false);
 
   useEffect(() => {
     // ログインチェック
-    if (user.id != '') {
-      setLogin(true)
+    if (user.id != "") {
+      setLogin(true);
 
       // 自分が投稿したブログチェック
       if (user.id === blog.profiles.id) {
-        setMyBlog(true)
+        setMyBlog(true);
       }
     }
-  }, [user])
+  }, [user]);
 
   // ブログ削除
   const deleteBlog = async () => {
-    setLoading(true)
+    setLoading(true);
 
     // supabaseブログ削除
-    const { error } = await supabase.from('blogs').delete().eq('id', blog.id)
+    const { error } = await supabase.from("blogs").delete().eq("id", blog.id);
 
     if (error) {
-      alert(error.message)
-      setLoading(false)
-      return
+      alert(error.message);
+      setLoading(false);
+      return;
     }
 
     // ファイル名取得
-    const fileName = blog.image_url.split('/').slice(-1)[0]
+    const fileName = blog.image_url.split("/").slice(-1)[0];
 
     // 画像を削除
-    await supabase.storage.from('blogs').remove([`${user.id}/${fileName}`])
+    await supabase.storage.from("blogs").remove([`${user.id}/${fileName}`]);
 
     // トップページに遷移
-    router.push(`/`)
-    router.refresh()
+    router.push(`/`);
+    router.refresh();
 
-    setLoading(false)
-  }
+    setLoading(false);
+  };
 
   // 自分が投稿したブログのみ、編集削除ボタンを表示
   const renderButton = () => {
@@ -73,7 +74,7 @@ const BlogDetail = ({ blog }: PageProps) => {
             <Loading />
           ) : (
             <div className="flex items-center space-x-2">
-              <Link href={`blog/${blog.id}/edit`}>
+              <Link href={`/blog/${blog.id}/edit`}>
                 <PencilSquareIcon className="h-5 w-5 text-green-500 hover:brightness-110" />
               </Link>
               <div className="cursor-pointer" onClick={() => deleteBlog()}>
@@ -82,16 +83,20 @@ const BlogDetail = ({ blog }: PageProps) => {
             </div>
           )}
         </div>
-      )
+      );
     }
-  }
+  };
 
   return (
     <div className="max-w-screen-md mx-auto">
       <div className="flex flex-col items-center justify-center mb-5">
         <div className="mb-1">
           <Image
-            src={blog.profiles.avatar_url ? blog.profiles.avatar_url : '/default.png'}
+            src={
+              blog.profiles.avatar_url
+                ? blog.profiles.avatar_url
+                : "/default.png"
+            }
             className="rounded-full"
             alt="avatar"
             width={70}
@@ -100,7 +105,7 @@ const BlogDetail = ({ blog }: PageProps) => {
         </div>
         <div className="font-bold text-gray-500">{blog.profiles.name}</div>
         <div className="text-sm text-gray-500">
-          {format(new Date(blog.created_at), 'yyyy/MM/dd HH:mm')}
+          {format(new Date(blog.created_at), "yyyy/MM/dd HH:mm")}
         </div>
       </div>
 
@@ -115,14 +120,16 @@ const BlogDetail = ({ blog }: PageProps) => {
             height={576}
           />
         </div>
-        <div className="leading-relaxed break-words whitespace-pre-wrap">{blog.content}</div>
+        <div className="leading-relaxed break-words whitespace-pre-wrap">
+          {blog.content}
+        </div>
       </div>
 
       {renderButton()}
 
       <BlogComment blog={blog} login={login} />
     </div>
-  )
-}
+  );
+};
 
-export default BlogDetail
+export default BlogDetail;
