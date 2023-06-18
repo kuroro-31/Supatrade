a07b6d87341f429faf3da954f87a70afcc4e5eea
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-18 11:34:15 +0900

    loading サイズを小さくする

--- app/loading.tsx
+++ app/loading.tsx
@@ -6,7 +6,7 @@
 const Loading = () => {
   return (
     <div className="flex justify-center">
-      <div className="h-10 w-10 animate-spin rounded-full border-2 border-primary border-t-transparent" />
+      <div className="h-8 w-8 animate-spin rounded-full border-2 border-primary border-t-transparent" />
     </div>
   );
 };
