e0305180834270583bcb60906b342b8807722a06
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-18 11:29:03 +0900

    copyright修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
 
             <footer className="py-5 border-t">
               <div className="text-center text-sm text-gray-500">
-                Copyright © All rights reserved | FullStackChannel
+                Copyright © All rights reserved | Startrade
               </div>
             </footer>
           </div>
