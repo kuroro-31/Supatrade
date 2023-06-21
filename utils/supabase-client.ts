8f4fc6b3aed28ad2fae8ecf8555a07051288b50a
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-21 19:37:19 +0900

    Supabaseの認証設定の修正

--- utils/supabase-client.ts
+++ utils/supabase-client.ts
@@ -3,13 +3,24 @@
 | Supabase Client
 |--------------------------------------------------------------------------
 */
-import { createClient } from '@supabase/supabase-js';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-  throw new Error("Missing Supabase environment variables");
+  throw new Error("Supabase environment variables are not set");
 }
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const options = {
+  auth: {
+    persistSession: true, // ユーザーがブラウザを閉じた後もセッションを維持するためのもの
+    storage: window.localStorage, // Supabaseの認証情報はデフォルトでlocalStorageに保存される
+  },
+};
+
+export const supabase: SupabaseClient = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY,
+  options
+);
