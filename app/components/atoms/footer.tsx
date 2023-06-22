a00efe6135b1a8f14b9dd9eee48b450f61b90847
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-23 08:27:50 +0900

    フッターの更新

--- app/components/atoms/footer.tsx
+++ app/components/atoms/footer.tsx
@@ -3,11 +3,94 @@
 | フッター
 |--------------------------------------------------------------------------
 */
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <footer className="py-5 border-t">
-      <div className="text-center text-sm text-gray-500">
-        Copyright © All rights reserved | Startrade
+    <footer className="w-full mb-16 md:mb-0 bg-f8 dark:bg-dark py-8 md:py-16 text-[#7c7c7c] dark:text-[#939599] dark:border-t dark:border-dark-1">
+      <div className="max-w-5xl mx-auto flex flex-col md:flex-row pb-8 md:pb-12 px-8 md:px-0 border-b border-[#7c7c7c] dark:border-dark-1">
+        <div className="md:w-2/5 flex flex-col items-start">
+          <Image
+            src="/logo.png"
+            alt="Supatrade Logo"
+            width={250}
+            height={40}
+            className="h-[35px] md:h-[40px]"
+          />
+        </div>
+        <div className="md:w-3/5 mt-8 md:mt-0 flex flex-col">
+          <div className="w-full flex flex-col md:flex-row justify-between">
+            <div className="md:w-1/2">
+              <div className="tracking-widest text-xl mb-4 cursor-default dark:text-[#c9cacc]">
+                ヘルプ
+              </div>
+              <a
+                href="/about/supatrade"
+                className="block mb-2 hover:text-primary dark:hover:text-ddd"
+              >
+                Supatradeについて
+              </a>
+              <a
+                href="/user_guide"
+                className="block mb-2 hover:text-primary dark:hover:text-ddd"
+              >
+                ご利用ガイド
+              </a>
+              <a
+                href="/faq?number=1"
+                className="block mb-2 hover:text-primary dark:hover:text-ddd"
+              >
+                よくあるご質問
+              </a>
+            </div>
+            <div className="md:w-1/2 mt-8 md:mt-0">
+              <div className="tracking-widest text-xl mb-4 cursor-default dark:text-[#c9cacc]">
+                利用規約とポリシー
+              </div>
+              <a
+                href="/terms_of_service"
+                className="block mb-2 hover:text-primary dark:hover:text-ddd"
+              >
+                利用規約
+              </a>
+              <a
+                href="/privacy_policy"
+                className="block mb-2 hover:text-primary dark:hover:text-ddd"
+              >
+                プライバシーポリシー
+              </a>
+              <a
+                href="/sct"
+                className="block mb-2 hover:text-primary dark:hover:text-ddd"
+              >
+                特定商取引法に基づく表記
+              </a>
+            </div>
+          </div>
+          <div className="mt-8 w-full flex flex-col md:flex-row justify-between">
+            <div className="md:w-1/2">
+              <div className="tracking-widest text-xl mb-4 cursor-default dark:text-[#c9cacc]">
+                ニュース
+              </div>
+              <span className="block mb-2 hover:text-primary dark:hover:text-ddd">
+                お知らせ(準備中)
+              </span>
+              <a
+                href="/release_note"
+                className="block mb-2 hover:text-primary dark:hover:text-ddd"
+              >
+                リリースノート
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="md:max-w-5xl mx-auto mt-8 md:mt-16 flex flex-col-reverse md:flex-row justify-between items-center">
+        <div className="md:w-2/5 mt-8 md:mt-0 tracking-widest cursor-default">
+          &copy;
+          {new Date().getFullYear()}
+          <span className="ml-2">Supatrade</span>
+        </div>
       </div>
     </footer>
   );
