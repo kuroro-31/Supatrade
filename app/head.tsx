98d49519b2f7cb41b1b4710e9f48bfe2236e89bf
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-22 20:10:19 +0900

    headの修正

--- app/head.tsx
+++ app/head.tsx
@@ -1,17 +1,19 @@
+import Head from "next/head";
+
 /*
 |--------------------------------------------------------------------------
 | Head
 |--------------------------------------------------------------------------
 */
-const Head = () => {
+const myHead = () => {
   return (
-    <>
+    <Head>
       <title>Supatrade</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Supatrade" />
       <link rel="icon" href="/favicon.ico" />
-    </>
+    </Head>
   );
 };
 
-export default Head;
+export default myHead;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import "server-only";
 import "../styles/globals.scss";
 
-import Head from "next/head";
-import { useRouter } from "next/router";
-
 import { Analytics } from "@vercel/analytics/react";
 
 import { supabase } from "../utils/supabase-client"; // 修正したインポート
 import SupabaseListener from "./components/supabase-listener";
 import SupabaseProvider from "./components/supabase-provider";
-import HeadContents from "./head";
+import Head from "./head";
 
 // キャッシュをしない
 export const revalidate = 0;
@@ -25,15 +22,9 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
     data: { session },
   } = await supabase.auth.getSession();
 
-  const router = useRouter();
-  const canonicalUrl = `${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`;
-
   return (
     <html>
-      <Head>
-        <HeadContents />
-        <link rel="canonical" href={canonicalUrl} />
-      </Head>
+      <Head />
 
       <body>
         <SupabaseProvider>
