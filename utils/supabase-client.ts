e4d21e003c84064b7605681af1139b684b6eda80
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-24 10:31:38 +0900

    supabase clientとserverのコメント追加

--- utils/supabase-client.ts
+++ utils/supabase-client.ts
@@ -1,9 +1,13 @@
 /*
 |--------------------------------------------------------------------------
 | Supabase Client
+| クライアント側でSupabaseを使用するための設定を提供します。
+| これは、ブラウザ上で実行されるJavaScriptコードで使用されます。
+| このファイルでは、createClient関数を使用してSupabaseのクライアントを作成しています。
 |--------------------------------------------------------------------------
 */
-import { createClient } from '@supabase/supabase-js';
+
+import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
--- utils/supabase-server.ts
+++ utils/supabase-server.ts
@@ -1,12 +1,15 @@
 /*
 |--------------------------------------------------------------------------
 | Supabase Server
+| サーバー側でSupabaseを使用するための設定を提供します。
+| これは、Next.jsのサーバーコンポーネントやAPIルートなど、サーバー上で実行されるJavaScriptコードで使用されます。
+| このファイルでは、createServerComponentClient関数を使用してSupabaseのクライアントを作成しています。
 |--------------------------------------------------------------------------
 */
-import { cookies } from 'next/headers';
+import { cookies } from "next/headers";
 
 // utils/supabase-server.ts
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 import type { Database } from "./database.types";
 
