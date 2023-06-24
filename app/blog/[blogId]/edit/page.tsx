ae591f5f4929f5019e5dc58d6647d1f3a3834b3a
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-24 15:00:49 +0900

    不要な処理を削除

--- app/blog/[blogId]/edit/page.tsx
+++ app/blog/[blogId]/edit/page.tsx
@@ -1,19 +1,9 @@
-import { useStore } from "../../../../store";
+// BlogEditPage.tsx
 import Footer from "../../../components/atoms/footer";
 import Header from "../../../components/atoms/header";
 import BlogEditForm from "../../../components/blog/edit/BlogEditForm";
 
 const BlogEditPage = ({ blog }: { blog: any }) => {
-  const { user } = useStore();
-
-  useEffect(() => {
-    // 自分が投稿したブログチェック
-    if (user?.id !== blog.profile_id) {
-      // ブログ詳細に遷移
-      window.location.href = `/blog/${blog.id}`;
-    }
-  }, [user?.id, blog.profile_id, blog.id]);
-
   return (
     <div className="">
       <Header />
@@ -24,6 +14,3 @@ const BlogEditPage = ({ blog }: { blog: any }) => {
 };
 
 export default BlogEditPage;
-function useEffect(arg0: () => void, arg1: any[]) {
-  throw new Error("Function not implemented.");
-}
