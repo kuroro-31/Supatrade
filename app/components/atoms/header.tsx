980a538df58ca565f678d424cc9c2f84796e1436
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-25 14:42:40 +0900

    ヘッダーとフッターの修正

--- app/components/atoms/header.tsx
+++ app/components/atoms/header.tsx
@@ -21,61 +21,59 @@ const Header = () => {
   }, []);
 
   return (
-    <header>
-      <div className="bg-white dark:bg-dark w-full flex-none border-b border-comiee">
-        <div className="max-w-8xl mx-auto">
-          <div className="py-4 lg:px-8 lg:border-0 mx-4 lg:mx-0">
-            <div className="relative flex items-center">
-              {/* ロゴ */}
-              <Link
-                href="/"
-                className="flex-none md:overflow-hidden md:w-auto"
-                passHref
-              >
-                <span className="sr-only">
-                  Startrade - Stock Trading Social Networking Service
-                </span>
-                <h1 className="dark:text-white">
-                  {/* <Logo /> */}
-                  <Image src="/logo.png" width={170} height={30} alt="" />
-                </h1>
-              </Link>
+    <header className="w-full flex-none border-b border-comiee">
+      <div className="container mx-auto">
+        <div className="py-4 lg:px-8 lg:border-0 mx-4 lg:mx-0">
+          <div className="relative flex items-center">
+            {/* ロゴ */}
+            <Link
+              href="/"
+              className="flex-none md:overflow-hidden md:w-auto"
+              passHref
+            >
+              <span className="sr-only">
+                Startrade - Stock Trading Social Networking Service
+              </span>
+              <h1 className="dark:text-white">
+                {/* <Logo /> */}
+                <Image src="/logo.png" width={170} height={30} alt="" />
+              </h1>
+            </Link>
 
-              {/* メニュー */}
-              <div className="flex items-center md:ml-auto">
-                <div className="hidden lg:flex items-center">
-                  <nav className="text-sm">
-                    <div className="flex items-center">
-                      {isClient &&
-                        (user ? (
-                          <div className="flex items-center">
-                            <BlogNewButton />
-                            <Link
-                              href="/auth/profile"
-                              className="ml-4 hover:text-primary"
-                            >
-                              プロフィール
-                            </Link>
-                          </div>
-                        ) : (
-                          <div className="flex items-center">
-                            <Link
-                              href="/auth/login"
-                              className="btn-primary hover:text-primary"
-                            >
-                              ログイン
-                            </Link>
-                            <Link
-                              href="/auth/signup"
-                              className="ml-4 hover:text-primary dark:hover:text-f5"
-                            >
-                              新規登録
-                            </Link>
-                          </div>
-                        ))}
-                    </div>
-                  </nav>
-                </div>
+            {/* メニュー */}
+            <div className="flex items-center md:ml-auto">
+              <div className="hidden lg:flex items-center">
+                <nav className="text-sm">
+                  <div className="flex items-center">
+                    {isClient &&
+                      (user ? (
+                        <div className="flex items-center">
+                          <BlogNewButton />
+                          <Link
+                            href="/auth/profile"
+                            className="ml-4 hover:text-primary"
+                          >
+                            プロフィール
+                          </Link>
+                        </div>
+                      ) : (
+                        <div className="flex items-center">
+                          <Link
+                            href="/auth/login"
+                            className="btn-primary hover:text-primary"
+                          >
+                            ログイン
+                          </Link>
+                          <Link
+                            href="/auth/signup"
+                            className="ml-4 hover:text-primary dark:hover:text-f5"
+                          >
+                            新規登録
+                          </Link>
+                        </div>
+                      ))}
+                  </div>
+                </nav>
               </div>
             </div>
           </div>
