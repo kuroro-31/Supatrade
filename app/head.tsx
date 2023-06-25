d1c7d3e17d0af7600458aed3614616d00c154bf8
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-25 18:59:26 +0900

    head更新

--- app/head.tsx
+++ app/head.tsx
@@ -17,8 +17,8 @@ const myHead = () => {
         content="Supatradeは、日本株の銘柄についての情報共有を目的としたSNSです。株式投資の情報収集にお役立てください。ただし、株価の上昇を保証するものではありませんので、責任は負いかねます。ご注意ください。"
       />
       {/* OGP */}
-      <meta property="og:url" content="/" />
-      <meta property="og:site_name" content="{{ config('app.name') }}" />
+      <meta property="og:url" content="https://www.supatrade.trade" />
+      <meta property="og:site_name" content="Supatrade" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta name="twitter:card" content="summary_large_image" />
