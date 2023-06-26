33c8a62f5a44166c5f2464eb681dbf609cef95b5
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-26 20:05:32 +0900

    ogp設定

--- app/head.tsx
+++ app/head.tsx
@@ -14,12 +14,22 @@ const myHead = () => {
         name="description"
         content="Supatradeは、日本株の情報共有を目的としたSNSです。Pythonによるテクニカル分析を用いて日本株の短期トレンドの株価予測を行っています。株価の上昇を保証するものではありませんので、責任は負いかねます。ご注意ください。"
       />
-      {/* OGP */}
+      <meta
+        property="og:description"
+        content="Supatradeは、日本株の情報共有を目的としたSNSです。Pythonによるテクニカル分析を用いて日本株の短期トレンドの株価予測を行っています。株価の上昇を保証するものではありませんので、責任は負いかねます。ご注意ください。"
+      />
+      <meta
+        name="twitter:description"
+        content="Supatradeは、日本株の情報共有を目的としたSNSです。Pythonによるテクニカル分析を用いて日本株の短期トレンドの株価予測を行っています。株価の上昇を保証するものではありませんので、責任は負いかねます。ご注意ください。"
+      />
       <meta property="og:url" content="https://www.supatrade.trade" />
       <meta property="og:site_name" content="Supatrade" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta name="twitter:card" content="summary_large_image" />
+      <meta property="og:image" content="/ogp.png" />
+      <meta property="og:image:secure_url" content="/ogp.png" />
+      <meta name="twitter:image" content="/ogp.png" />
 
       <title>Supatrade | 日本株の情報共有サイト</title>
 
