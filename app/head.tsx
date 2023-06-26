d480b0916e6f03f5b55c1ae55631e902d5459d18
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-26 14:09:56 +0900

    ページ説明更新

--- app/head.tsx
+++ app/head.tsx
@@ -7,14 +7,12 @@ const myHead = () => {
   return (
     // eslint-disable-next-line @next/next/no-head-element
     <head>
-      <title>Supatrade | 日本株の情報共有サイト</title>
-
       <meta charSet="utf-8" />
       <meta name="author" content="Supatrade" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
         name="description"
-        content="Supatradeは、日本株の銘柄についての情報共有を目的としたSNSです。株式投資の情報収集にお役立てください。ただし、株価の上昇を保証するものではありませんので、責任は負いかねます。ご注意ください。"
+        content="Supatradeは、日本株の情報共有を目的としたSNSです。Pythonによるテクニカル分析を用いて日本株の短期トレンドの株価予測を行っています。株価の上昇を保証するものではありませんので、責任は負いかねます。ご注意ください。"
       />
       {/* OGP */}
       <meta property="og:url" content="https://www.supatrade.trade" />
@@ -22,6 +20,8 @@ const myHead = () => {
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta name="twitter:card" content="summary_large_image" />
+
+      <title>Supatrade | 日本株の情報共有サイト</title>
 
       <link rel="icon" href="/favicon.ico" />
     </head>
