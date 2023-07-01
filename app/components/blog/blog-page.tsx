0abaf0b86f835950fb2fd1264a22920aca3d9250
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-07-02 01:39:33 +0900

    エラー回避

--- app/components/blog/blog-page.tsx
+++ app/components/blog/blog-page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { NextRouter, useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import Footer from "../atoms/footer";
 import Header from "../atoms/header";
@@ -12,13 +12,14 @@ type PageProps = {
 
 // ブログ詳細
 const BlogPage = ({ blogId }: PageProps) => {
-  const router: NextRouter = useRouter();
+  const router = useRouter();
   return (
     <div>
       <Header />
       <div className="container w-full flex justify-between mx-auto">
         <div className="hidden lg:block lg:w-1/4"></div>
         <main className="w-full lg:w-2/4">
+          {/* @ts-ignore */}
           <BlogDetail blogId={blogId} router={router} />
         </main>
         <div className="hidden lg:block lg:w-1/4"></div>
