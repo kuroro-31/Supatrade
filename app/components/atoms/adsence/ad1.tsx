e729f5e5cccd887b545c4b9243cb8222b106c892
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-09-01 12:03:15 +0900

    広告 スタイル修正

--- app/components/atoms/adsence/ad1.tsx
+++ app/components/atoms/adsence/ad1.tsx
@@ -3,7 +3,7 @@ function Ad1() {
     <div
       dangerouslySetInnerHTML={{
         __html: `
-          <a href="https://px.a8.net/svt/ejp?a8mat=3TFIRO+ALMVJM+ONS+TTDZ5" rel="nofollow">
+          <a href="https://px.a8.net/svt/ejp?a8mat=3TFIRO+ALMVJM+ONS+TTDZ5" rel="nofollow" class="w-full flex justify-center">
           <img border="0" width="234" height="60" alt="" src="https://www22.a8.net/svt/bgt?aid=230831556641&wid=001&eno=01&mid=s00000003196005008000&mc=1"></a>
           <img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=3TFIRO+ALMVJM+ONS+TTDZ5" alt="">
         `,
--- app/components/atoms/adsence/ntt_docome.tsx
+++ app/components/atoms/adsence/ntt_docome.tsx
@@ -3,7 +3,7 @@ function NttDocomo() {
     <div
       dangerouslySetInnerHTML={{
         __html: `
-          <a href="https://px.a8.net/svt/ejp?a8mat=3TFIRM+AI29WY+5244+609HT" rel="nofollow">
+          <a href="https://px.a8.net/svt/ejp?a8mat=3TFIRM+AI29WY+5244+609HT" rel="nofollow" class="w-full flex justify-center">
             <img border="0" width="234" height="60" alt="" src="https://www25.a8.net/svt/bgt?aid=230831554635&wid=001&eno=01&mid=s00000023602001009000&mc=1">
           </a>
           <img border="0" width="1" height="1" src="https://www19.a8.net/0.gif?a8mat=3TFIRM+AI29WY+5244+609HT" alt="">
